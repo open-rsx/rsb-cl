f8a6b711a4cc5349999d1f359cae759ef074a0ba
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-08-02 05:34:00 +0000

    Fixed in a docstring in src/protocol.lisp * src/protocol.lisp (receive): fixed typos in documentation string

--- src/protocol.lisp
+++ src/protocol.lisp
@@ -153,10 +153,10 @@ function."))
 		     &key
 		     block?)
   (:documentation
-   "Receive data from the channel in which RECEIVER is
+   "Receive data from the channel in which READER is
 participating. When data is received, it is returned in form of an
 `event' instance. If BLOCK? is non-nil, wait for data to become
-available if there is none. If BLOCK/ is nil and no data is available,
+available if there is none. If BLOCK? is nil and no data is available,
 nil is returned."))
 
 
