1111216f556070ce29685ae6fdfb5dc0ae55b566
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-09-17 17:36:39 +0000

    Simplification in src/transport/spread/package.lisp * src/transport/spread/package.lisp (package rsb.transport.spread):   qualify `defpackage' form with package instead of using an   additional `in-package' form

--- src/transport/spread/package.lisp
+++ src/transport/spread/package.lisp
@@ -17,9 +17,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses>.
 
-(cl:in-package :cl-user)
-
-(defpackage :rsb.transport.spread
+(cl:defpackage :rsb.transport.spread
   (:nicknames :rsb.tp.spread)
 
   (:shadowing-import-from :rsb.protocol
