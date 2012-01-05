8908e77503047bfa64acf0fa4de36f98e05c0c55
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-01-05 21:16:15 +0000

    Changed license GPLv3 -> LGPLv3 fixes #790 * **/*.lisp (header): updated copyright; changed license information   GPLv3 -> LGPLv3 * cl-rsb.asd: likewise * CMakeLists.txt (header): added header including license information * COPYING: changed GPLv3 -> LGPLv3

--- src/filter/scope-filter.lisp
+++ src/filter/scope-filter.lisp
@@ -1,21 +1,26 @@
 ;;; scope-filter.lisp ---
 ;;
-;; Copyright (C) 2011 Jan Moringen
+;; Copyright (C) 2011, 2012 Jan Moringen
 ;;
 ;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 ;;
-;; This Program is free software: you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This file may be licensed under the terms of of the GNU Lesser
+;; General Public License Version 3 (the ``LGPL''), or (at your
+;; option) any later version.
 ;;
-;; This Program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
+;; Software distributed under the License is distributed on an ``AS
+;; IS'' basis, WITHOUT WARRANTY OF ANY KIND, either express or
+;; implied. See the LGPL for the specific language governing rights
+;; and limitations.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <http://www.gnu.org/licenses>.
+;; You should have received a copy of the LGPL along with this
+;; program. If not, go to http://www.gnu.org/licenses/lgpl.html or
+;; write to the Free Software Foundation, Inc., 51 Franklin Street,
+;; Fifth Floor, Boston, MA 02110-1301, USA.
+;;
+;; The development of this software was supported by:
+;;   CoR-Lab, Research Institute for Cognition and Robotics
+;;     Bielefeld University
 
 (in-package :rsb.filter)
 
