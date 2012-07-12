db4a256b54ff4bc5628b308106f37f877b3971df
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-07-12 17:29:48 +0000

    Fixed treatment of CPACK_GENERATOR in cpack/CPackInclude.cmake * cpack/CPackInclude.cmake: do not overwrite CPACK_GENERATOR if it   is already set

--- cpack/CPackInclude.cmake
+++ cpack/CPackInclude.cmake
@@ -1,4 +1,6 @@
-SET(CPACK_GENERATOR "TGZ")
+IF(NOT CPACK_GENERATOR)
+    SET(CPACK_GENERATOR "TGZ")
+ENDIF()
 
 SET(CPACK_CONFIG_FILE "" CACHE FILEPATH "Path to a CMake lists syntax file providing settings for CPack.")
 SET(CPACK_PACKAGE_REVISION "" CACHE STRING "A suffix string which can be appended to package versions to account for e. g. multiple rebuilds without changes to the upstream project of the package.")
