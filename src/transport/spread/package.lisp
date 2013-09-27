c12d07d8a05d2448ed310fca1d6b1343a97ecd02
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2013-09-27 23:35:26 +0200

    Shortened license, updated copyright in **/*.lisp, cl-rsb.asd, CMakeLists.txt
    * **/*.lisp (header): shortened license; updated copyright
    * cl-rsb.asd: likewise
    * CMakeLists.txt: likewise

--- src/transport/spread/package.lisp
+++ src/transport/spread/package.lisp
@@ -1,26 +1,8 @@
-;;; package.lisp --- Package definition for transport.spread module.
-;;
-;; Copyright (C) 2011, 2012 Jan Moringen
-;;
-;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
-;;
-;; This file may be licensed under the terms of of the GNU Lesser
-;; General Public License Version 3 (the ``LGPL''), or (at your
-;; option) any later version.
-;;
-;; Software distributed under the License is distributed on an ``AS
-;; IS'' basis, WITHOUT WARRANTY OF ANY KIND, either express or
-;; implied. See the LGPL for the specific language governing rights
-;; and limitations.
-;;
-;; You should have received a copy of the LGPL along with this
-;; program. If not, go to http://www.gnu.org/licenses/lgpl.html or
-;; write to the Free Software Foundation, Inc., 51 Franklin Street,
-;; Fifth Floor, Boston, MA 02110-1301, USA.
-;;
-;; The development of this software was supported by:
-;;   CoR-Lab, Research Institute for Cognition and Robotics
-;;     Bielefeld University
+;;;; package.lisp --- Package definition for transport.spread module.
+;;;;
+;;;; Copyright (C) 2011, 2012, 2013 Jan Moringen
+;;;;
+;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:defpackage :rsb.transport.spread
   (:nicknames :rsb.tp.spread)
@@ -38,9 +20,9 @@
    :iterate
    :let-plus
    :more-conditions
-   
+
    :nibbles
-   
+
    :rsb
    :rsb.event-processing
    :rsb.transport
@@ -67,7 +49,7 @@
   ;; Variables and constants
   (:export
    :+protocol-version+)
-  
+
   (:documentation
    "This package contains a transport implementation based on the
 spread group communication system."))
