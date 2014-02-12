b6aa36fd339b881933ac64be734c2983de949acc
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-02-12 10:13:02 +0100

    Cleanup in src/patterns/request-reply/future.lisp
    * src/patterns/request-reply/future.lisp (future-failure): new
      structure; new data structure for representing future failures
      (+future-failure-marker+): removed; replace by `future-failure'
      (future-failure-value): likewise
      (future-failure-tag): likewise
      (future-failure-condition): likewise
      (future::result): fixed writer name setf {%future-result ->
      future-%result}
      (future::lock): similar
      (future::condition): similar
      (future-result future): adapted to `future-failure' structure and
      renamed accessors
      (setf future-result t future): likewise
      (setf future-error t future): likewise
      (%make-failure-value): removed; replace by `future-failure'

--- src/patterns/request-reply/future.lisp
+++ src/patterns/request-reply/future.lisp
@@ -6,46 +6,29 @@
 
 (cl:in-package #:rsb.patterns.request-reply)
 
-;;; Representation of errors
+;;; Representation of failures
 
-(defconstant +future-failure-marker+
-  (if (boundp '+future-failure-marker+)
-      (symbol-value '+future-failure-marker+)
-      (gensym "FUTURE-FAILURE-MARKER"))
-  "This unique object is used to mark failure values as such and
-distinguish them from regular results.")
-
-(deftype future-failure-value ()
-  "The extension of this type consists of all failure values."
-  `(cons (eql ,+future-failure-marker+)
-         (cons keyword
-               (cons t null))))
-
-(declaim (inline future-failure-tag future-failure-condition))
-
-(defun future-failure-tag (value)
-  "Return the tag of a failure value."
-  (second value))
-
-(defun future-failure-condition (value)
-  "Return the condition data of a failure value."
-  (third value))
+(defstruct (future-failure
+            (:constructor make-future-failure (tag condition))
+            (:copier      nil))
+  (tag       nil :type keyword :read-only t)
+  (condition nil :type list    :read-only t))
 
 ;;; `future' class
 
 (defclass future ()
   ((result    :initarg  :result
-              :writer   (setf %future-result)
+              :writer   (setf future-%result)
               :documentation
               "Stores the result of the operation associated to the
 future. Remains unbound until the operation completes successfully or
 fails.")
-   (lock      :reader   %future-lock
+   (lock      :reader   future-%lock
               :initform (bt:make-lock "Future lock")
               :documentation
               "Stores the lock that protects access to the result
 slot.")
-   (condition :reader   %future-condition
+   (condition :reader   future-%condition
               :initform (bt:make-condition-variable
                          :name "Future condition")
               :documentation
@@ -76,12 +59,12 @@ condition objects have to be available to some code outside the future
 or for performance reasons."))
 
 (defmethod future-done? ((future future))
-  (bt:with-lock-held ((%future-lock future))
+  (bt:with-lock-held ((future-%lock future))
     (when (slot-boundp future 'result)
       (let ((value (slot-value future 'result)))
         ;; When there is some result, check whether it indicates an
         ;; error.
-        (if (typep value 'future-failure-value)
+        (if (future-failure-p value)
             (future-failure-tag value)
             :done)))))
 
@@ -119,8 +102,8 @@ or for performance reasons."))
                           &key
                           (error? t)
                           &allow-other-keys)
-  (let+ (((&accessors-r/o (lock      %future-lock)
-                          (condition %future-condition)) future)
+  (let+ (((&accessors-r/o (lock      future-%lock)
+                          (condition future-%condition)) future)
          (value (progn
                   (bt:with-lock-held (lock)
                     (iter (until (slot-boundp future 'result))
@@ -129,7 +112,7 @@ or for performance reasons."))
     ;; Return the result stored in VALUE or signal an error, depending
     ;; on ERROR?
     (cond
-      ((not (typep value 'future-failure-value))
+      ((not (future-failure-p value))
        (values value :done))
 
       ;; When the stored value indicates an error, signal the error or
@@ -141,24 +124,17 @@ or for performance reasons."))
        (values nil (future-failure-tag value))))))
 
 (defmethod (setf future-result) ((new-value t) (future future))
-  (bt:with-lock-held ((%future-lock future))
+  (bt:with-lock-held ((future-%lock future))
     (prog1
-        (setf (%future-result future) new-value)
-      (bt:condition-notify (%future-condition future)))))
+        (setf (future-%result future) new-value)
+      (bt:condition-notify (future-%condition future)))))
 
 (defmethod (setf future-error) ((new-value t) (future future))
   ;; Store a result value that indicates an error. NEW-VALUE can be
   ;; used to (construct and) signal the error.
-  (setf (future-result future) (%make-failure-value
-                                (ensure-list new-value))))
+  (setf (future-result future)
+        (make-future-failure :failed (ensure-list new-value))))
 
 (defmethod print-object ((object future) stream)
   (print-unreadable-object (object stream :type t :identity t)
     (format stream "~A" (or (future-done? object) :running))))
-
-;;; Utility functions
-
-(defun %make-failure-value (condition-data)
-  "Return an object that can be used to indicate a failed operation
-for a condition described by CONDITION-DATA."
-  (list +future-failure-marker+ :failed condition-data))
