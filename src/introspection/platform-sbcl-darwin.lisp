9042517c21fe5e9e30e82ad2af1bb8cae3b43dbb
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-12-18 19:41:37 +0100

    Implemented %current-process-start-time in src/introspection/platform-sbcl-darwin.lisp
    refs #1823
    
    * src/introspection/platform-sbcl-darwin.lisp
      (define-alien-type struct kproc): new alien structure; used by
      "sysctl" syscall to return information about a process
      (%current-process-start-time): call "sysctl" for the "kern.proc.pid"
      mib and extract the start time

--- src/introspection/platform-sbcl-darwin.lisp
+++ src/introspection/platform-sbcl-darwin.lisp
@@ -14,10 +14,42 @@
 (defun current-program-name-and-commandline-arguments ()
   sb-ext:*posix-argv*)
 
+;; This is not the actual definition of the kproc structure, but we
+;; are only interested in the start time.
+(sb-alien:define-alien-type nil
+  (sb-alien:struct kproc
+    (start-time (sb-alien:struct sb-unix:timeval))
+    (padding    (array sb-alien:char 632))))
+
 (defun %current-process-start-time ()
   (with-platform-information-error-translation
       ("determine start time of the current process")
-    (error "~@<Not available.~@:>")))
+    (sb-alien:with-alien ((mib      (array sb-alien:int 16))
+                          (mib-size sb-alien:size-t         16))
+      ;; Translate the string name into a sequence of numeric ids.
+      (sb-unix::syscall* ("sysctlnametomib" sb-alien:c-string (* sb-alien:int) (* sb-alien:size-t))
+          (values)
+        "kern.proc.pid"
+        (sb-alien:cast mib (* sb-alien:int)) (sb-alien:addr mib-size))
+      ;; Add the process id as the fourth element of the mib. Increase
+      ;; the size correspondingly.
+      (setf (sb-alien:deref mib mib-size) (sb-posix:getpid))
+      (incf mib-size)
+      ;; Return information block and extract start time.
+      (sb-alien:with-alien
+          ((kproc      (sb-alien:struct kproc))
+           (kproc-size sb-alien:size-t         (sb-alien:alien-size
+                                                (sb-alien:struct kproc))))
+        (sb-unix::syscall* ("sysctl" (* sb-alien:int) sb-alien:size-t
+                                     (* t) (* sb-alien:size-t)
+                                     (* t) sb-alien:size-t)
+            (let* ((start-time    (sb-alien:slot kproc 'start-time))
+                   (seconds       (sb-alien:slot start-time 'sb-unix::tv-sec))
+                   (micro-seconds (sb-alien:slot start-time 'sb-unix::tv-usec)))
+              (local-time:unix-to-timestamp seconds :nsec (* 1000 micro-seconds)))
+          (sb-alien:cast mib (* sb-alien:int)) mib-size
+          (sb-alien:addr kproc) (sb-alien:addr kproc-size)
+          (sb-sys:int-sap 0) 0)))))
 
 ;;; Host information
 
