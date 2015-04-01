5689482f4dde077e6ad9f84958519d6fd1b51df3
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-04-01 21:08:26 +0200

    Accessors for slots in src/transport/spread/connection.lisp
    * src/transport/spread/connection.lisp (header): updated copyright
      (connection::connection): added accessor
      (connection::groups): likewise
      (connection::receive-buffer): likewise
      (shared-initialize :after connection t): use accessors
      (ref-group connection string): likewise
      (unref-group connection string): likewise
      (receive-message connection t): likewise
      (send-message connection list simple-array): likewise
      (print-object connection t): likewise
      (%ensure-receive-buffer): likewise

--- src/transport/spread/connection.lisp
+++ src/transport/spread/connection.lisp
@@ -1,6 +1,6 @@
 ;;;; connection.lisp --- Spread connections with membership management.
 ;;;;
-;;;; Copyright (C) 2011, 2012, 2013, 2014 Jan Moringen
+;;;; Copyright (C) 2011, 2012, 2013, 2014, 2015 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -35,25 +35,30 @@ GROUP in case of a 1 -> 0 transition."))
 (defclass connection ()
   ((connection     :initarg  :connection
                    :type     network.spread:connection
+                   :accessor connection-%connection
                    :documentation
-                   "The underlying spread connection used by the connector
-instance.")
+                   "The underlying Spread connection used by the
+                    connector instance.")
    (groups         :type     hash-table
+                   :reader   connection-%groups
                    :initform (make-hash-table :test #'equal)
                    :documentation
-                   "A mapping of group names to reference counts. The
-connection instance is a member of every group that has a positive
-reference count.")
+                   "A mapping of group names to reference counts.
+
+                    The connection instance is a member of every group
+                    that has a positive reference count.")
    (receive-buffer :type     (or null simple-octet-vector)
+                   :accessor connection-%receive-buffer
                    :initform nil
                    :documentation
-                   "Stores a buffer which is used for receiving
-messages. The buffer is allocated lazily to avoid wasting memory for
-send-only connections."))
+                   "Stores a buffer for receiving messages.
+
+                    The buffer is allocated lazily to avoid wasting
+                    memory for send-only connections."))
   (:documentation
-   "Instances of this class represent connections to Spread
-communication system and maintain a list of the Spread multicast
-groups in which they are members."))
+   "Instances of this class represent connections to the Spread
+    communication system and maintain a list of the Spread multicast
+    groups in which they are members."))
 
 (defmethod initialize-instance :before ((instance connection)
                                         &key
@@ -77,26 +82,29 @@ groups in which they are members."))
                                      &key
                                      name)
   (when name
-    (setf (slot-value instance 'connection)
+    (setf (connection-%connection instance)
           (network.spread:connect name))))
 
 (defmethod ref-group ((connection connection) (group string))
-  (when (= (incf (gethash group (slot-value connection 'groups) 0)) 1)
-    (log:info "~@<~A is joining group ~A~@:>"
-              connection group)
-    (network.spread:join (slot-value connection 'connection) group)))
+  (let+ (((&structure-r/o connection- %connection (groups %groups))
+          connection))
+    (when (= (incf (gethash group groups 0)) 1)
+      (log:info "~@<~A is joining group ~A~@:>" connection group)
+      (network.spread:join %connection group))))
 
 (defmethod unref-group :around ((connection connection) (group string))
-  (if (gethash group (slot-value connection 'groups))
+  (if (gethash group (connection-%groups connection))
       (call-next-method)
       (log:warn "~@<~A was asked to unreference unknown group ~A~@:>"
                 connection group)))
 
 (defmethod unref-group ((connection connection) (group string))
-  (when (zerop (decf (gethash group (slot-value connection 'groups) 0)))
-    (log:info "~@<~A is leaving group ~A~@:>" connection group)
-    (remhash group (slot-value connection 'groups))
-    (network.spread:leave (slot-value connection 'connection) group)))
+  (let+ (((&structure-r/o connection- %connection (groups %groups))
+          connection))
+    (when (zerop (decf (gethash group groups 0)))
+      (log:info "~@<~A is leaving group ~A~@:>" connection group)
+      (remhash group groups)
+      (network.spread:leave %connection group))))
 
 (macrolet
     ((with-spread-condition-translation (&body body)
@@ -112,15 +120,15 @@ groups in which they are members."))
 
   (defmethod receive-message ((connection connection) (block? t))
     (declare (inline %ensure-receive-buffer))
-    (let+ ((buffer (%ensure-receive-buffer connection))
-           ((&slots-r/o connection) connection))
+    (let+ (((&accessors-r/o (connection connection-%connection)
+                            (buffer     %ensure-receive-buffer))
+            connection))
       (with-spread-condition-translation
-        (values
-         buffer
-         (network.spread:receive-into connection buffer
-                                      :block?         block?
-                                      :return-sender? nil
-                                      :return-groups? nil)))))
+        (values buffer
+                (network.spread:receive-into connection buffer
+                                             :block?         block?
+                                             :return-sender? nil
+                                             :return-groups? nil)))))
 
   (defmethod send-message ((connection  connection)
                            (destination list)
@@ -128,20 +136,21 @@ groups in which they are members."))
     (check-type payload simple-octet-vector "a simple-octet-vector")
 
     (with-spread-condition-translation
-        (network.spread:send-bytes
-         (slot-value connection 'connection) destination payload))))
+       (network.spread:send-bytes
+        (connection-%connection connection) destination payload))))
 
 (defmethod print-object ((object connection) stream)
-  (with-slots (connection groups) object
-    (print-unreadable-object (object stream :type t :identity t)
-      (format stream "~A (~D)"
-              (network.spread:connection-name connection)
-              (hash-table-count groups)))))
+  (print-unreadable-object (object stream :type t :identity t)
+    (let+ (((&structure-r/o connection- %connection %groups) object))
+     (format stream "~A (~D)"
+             (network.spread:connection-name %connection)
+             (hash-table-count %groups)))))
 
 ;;; Utility functions
 
 (defun %ensure-receive-buffer (connection)
-  (or (slot-value connection 'receive-buffer)
-      (setf (slot-value connection 'receive-buffer)
-            (make-octet-vector
-             network.spread:+maximum-message-data-length+))))
+  (let+ (((&structure connection- %receive-buffer) connection))
+    (or %receive-buffer
+        (setf %receive-buffer
+              (make-octet-vector
+               network.spread:+maximum-message-data-length+)))))
