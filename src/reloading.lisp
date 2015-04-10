297460cccd04fc3302b8c7946b5ab7dbdd0d1b7d
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-04-10 17:38:20 +0200

    Use uiop:register-image-restore-hook in src/reloading.lisp
    * src/reloading.lisp (header): updated copyright
      (reseed-id-random-state): new function; reseed random-state used to
      generate participant ids
      (toplevel): register `reseed-id-random-state' as image restore hook

--- src/reloading.lisp
+++ src/reloading.lisp
@@ -1,17 +1,17 @@
 ;;;; reloading.lisp --- Code to be executed at startup/shutdown.
 ;;;;
-;;;; Copyright (C) 2013 Jan Moringen
+;;;; Copyright (C) 2013, 2015 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:rsb)
 
-(defun enable-id-random-state-reseed ()
-  "Reseed the `random-state' stored in `*id-random-state*'.
+;; Reseed the `random-state' stored in `*id-random-state*'.
+;;
+;; This code ensures that `*id-random-state*' is reseeded at image
+;; startup thereby ensuring different pseudo random ids and such for
+;; subsequent program runs.
+(defun reseed-id-random-state ()
+  (setf *id-random-state* (make-random-state t)))
 
-This function can be called to ensure that `*id-random-state*' is
-reseeded at image startup thereby ensuring different pseudo random ids
-and such for subsequent program runs."
-  #+sbcl
-  (push (lambda () (setf *id-random-state* (make-random-state t)))
-        sb-ext:*init-hooks*))
+(uiop:register-image-restore-hook 'reseed-id-random-state nil)
