9dba47a1c8a3733c69371f431652995af857004e
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2015-04-23 04:29:59 +0200

    Interruption-free termination in src/transport/spread/in-push-connector.lisp
    When detaching the connector, close the Spread connection before
    stopping the receiver thread. This lets the receiver thread notice the
    closed connection and return on its own instead of being interrupted.
    
    * src/transport/spread/in-push-connector.lisp
      (in-push-connector::state): new slot; stores the current state of the
      connector
      (apply-error-policy in-push-connector connection-unexpectedly-closed):
      new method; when shutting down, exit receiver thread
      (apply-error-policy in-push-connector spread-error): likewise
      (notify in-push-connector scope eql :attached): removed :after
      qualifier; set connector state to :active
      (notify in-push-connector scope eql :detached): removed :before
      qualifier; set connector state to :shutdown, stop receiver, set state
      to :inactive
      (receive-messages :around in-push-connector): during shutdown handle
      `spread-error' conditions by exiting the receiver thread

--- src/transport/spread/in-push-connector.lisp
+++ src/transport/spread/in-push-connector.lisp
@@ -14,24 +14,46 @@
 (defclass in-push-connector (in-connector
                              threaded-message-receiver-mixin
                              error-handling-push-receiver-mixin)
-  ()
+  ((state :type     (member :inactive :active :shutdown)
+          :accessor connector-%state
+          :initform :active
+          :documentation
+          "Stores the state of the connector. Currently only used
+           during shutdown."))
   (:metaclass connector-class)
   (:direction :in-push)
   (:documentation
    "This connector class implements push-style event receiving for the
 Spread transport."))
 
-(defmethod notify :after ((connector in-push-connector)
-                          (scope     scope)
-                          (action    (eql :attached)))
-  "After attaching to SCOPE, start a receiver thread."
+(defmethod apply-error-policy ((processor in-push-connector)
+                               (condition connection-unexpectedly-closed))
+  (if (eq (connector-%state processor) :shutdown)
+      (exit-receiver)
+      (call-next-method)))
+
+(defmethod apply-error-policy ((processor in-push-connector)
+                               (condition network.spread:spread-error))
+  (if (eq (connector-%state processor) :shutdown)
+      (exit-receiver)
+      (call-next-method)))
+
+(defmethod notify ((connector in-push-connector)
+                   (scope     scope)
+                   (action    (eql :attached)))
+  ;; When attaching to SCOPE, start a receiver thread.
+  (call-next-method)
+  (setf (connector-%state connector) :active)
   (start-receiver connector))
 
-(defmethod notify :before ((connector in-push-connector)
-                           (scope     scope)
-                           (action    (eql :detached)))
-  "Before detaching from SCOPE, join the receiver thread."
-  (stop-receiver connector))
+(defmethod notify ((connector in-push-connector)
+                   (scope     (eql t))
+                   (action    (eql :detached)))
+  ;; When detaching from SCOPE, join the receiver thread.
+  (setf (connector-%state connector) :shutdown)
+  (call-next-method)
+  (stop-receiver connector)
+  (setf (connector-%state connector) :inactive))
 
 (defmethod receive-messages :around ((connector in-push-connector))
   "Create a thread-local scope->groups cache for this connector."
