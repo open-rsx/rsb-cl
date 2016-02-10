077fd8b4e13220acafcd6edaf12e017f9cfeb0ea
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-02-10 13:29:19 +0100

    Fixed construction test in test/transport/connector-class.lisp
    * test/transport/connector-class.lisp (header): updated copyright
      (test connector-class-root::construction/invalid): try to undo side
      effects of test

--- test/transport/connector-class.lisp
+++ test/transport/connector-class.lisp
@@ -1,6 +1,6 @@
 ;;;; connector-class.lisp --- Unit tests for the connector-class class.
 ;;;;
-;;;; Copyright (C) 2011, 2012, 2013 Jan Moringen
+;;;; Copyright (C) 2011-2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -41,18 +41,22 @@
 (addtest (connector-class-root
           :documentation
           "Test error behavior for an invalid constructing an instance
-of `connector-class'.")
+           of `connector-class'.")
   construction/invalid
 
   ;; We request the option no-such-option to be constructed from the
   ;; slot of the same name. Since there is no such slot, an error has
   ;; to be signaled.
-  (ensure-condition 'error
-    (eval '(progn
-            (defclass bar ()
-              ()
-              (:metaclass connector-class)
-              (:options
-               (:no-such-option &slot)))
-            (closer-mop:finalize-inheritance
-             (find-class 'bar))))))
+  (eval `(defclass bar ()
+           ()
+           (:metaclass connector-class)
+           (:options
+            (:no-such-option &slot))))
+  (unwind-protect
+       (ensure-condition 'error
+         (closer-mop:finalize-inheritance (find-class 'bar)))
+    (eval `(defclass bar ()
+             ()
+             (:metaclass connector-class)
+             (:options)))
+    (setf (find-class 'bar) nil)))
