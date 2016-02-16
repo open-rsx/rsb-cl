51ed6f61586a0a9acfd15e29299108dabf02fe02
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-02-16 12:52:52 +0100

    Cosmetic changes in src/event-processing/broadcast-processor.lisp
    * src/event-processing/broadcast-processor.lisp (header): updated
      copyright
      (broadcast-processor): fixed indentation of documentation string
      (handle broadcast-processor t): turned documentation string into
      comment
      (dispatch broadcast-processor t): likewise

--- src/event-processing/broadcast-processor.lisp
+++ src/event-processing/broadcast-processor.lisp
@@ -1,10 +1,12 @@
 ;;;; broadcast-processor.lisp --- Broadcast events to multiple handlers.
 ;;;;
-;;;; Copyright (C) 2011, 2012, 2013 Jan Moringen
+;;;; Copyright (C) 2011-2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:rsb.event-processing)
+
+;;; `broadcast-processor'
 
 (defclass broadcast-processor ()
   ((handlers :initarg  :handlers
@@ -15,18 +17,20 @@
              "Stores a list of handlers to which events are dispatched
               via `handle'."))
   (:documentation
-   "Instances of this class maintain a list of handlers and dispatch
-events to these handlers. Methods on `handle' can be use to filter or
-transform events. Methods on `dispatch' can be used to modify
-dispatching behavior."))
+   "Instances maintain a list of handlers and dispatch events to these
+    handlers.
+
+    Methods on `handle' can be use to filter or transform
+    events. Methods on `dispatch' can be used to modify dispatching
+    behavior."))
 
 (defmethod handle ((processor broadcast-processor)
                    (data      t))
-  "Dispatch DATA to handlers of PROCESSOR in a manner defined by
-methods on `dispatch'."
+  ;; Dispatch DATA to handlers of PROCESSOR in a manner defined by
+  ;; methods on `dispatch'.
   (dispatch processor data))
 
 (defmethod dispatch ((processor broadcast-processor)
                      (data      t))
-  "Dispatch DATA to handlers of PROCESSOR."
+  ;; Dispatch DATA to handlers of PROCESSOR.
   (handle (handlers processor) data))
