0c423593ae77154dea8fae5a7be91b87c9cb3ad4
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-02-25 15:52:07 +0100

    Added superclass payload-matching-mixin in src/filter/type-filter.lisp
    * src/filter/type-filter.lisp (header): updated copyright
      (type-filter): added superclass `payload-matching-mixin'
      (matches? type-filter event): removed; replaced by `payload-matches'
      (payload-matches? type-filter t): new method; match payload against
      specified type

--- src/filter/type-filter.lisp
+++ src/filter/type-filter.lisp
@@ -1,12 +1,13 @@
 ;;;; type-filter.lisp --- A filter that discriminates event based on their type.
 ;;;;
-;;;; Copyright (C) 2011, 2012, 2013, 2014, 2015 Jan Moringen
+;;;; Copyright (C) 2011-2016 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
 (cl:in-package #:rsb.filter)
 
-(defclass type-filter (funcallable-filter-mixin)
+(defclass type-filter (funcallable-filter-mixin
+                       payload-matching-mixin)
   ((type :initarg  :type
          :type     (or list symbol)
          :accessor filter-type
@@ -21,8 +22,8 @@
 (service-provider:register-provider/class 'filter :type
   :class 'type-filter)
 
-(defmethod matches? ((filter type-filter) (event event))
-  (typep (event-data event) (filter-type filter)))
+(defmethod payload-matches? ((filter type-filter) (payload t) &key)
+  (typep payload (filter-type filter)))
 
 (defmethod print-object ((object type-filter) stream)
   (print-unreadable-object (object stream :type t :identity t)
