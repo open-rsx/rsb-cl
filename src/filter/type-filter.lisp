296836aabb585b0740cf715e89a7a16ab17ec4c3
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-02-25 22:37:45 +0100

    Compile type predicate in src/filter/type-filter.lisp
    * src/filter/type-filter.lisp (type-filter::type): removed initarg
      (type-filter::predicate): new slot; stores compiled predicate
      (shared-initialize :after type-filter t): new method; set type slot
      using writer to trigger predicate compilation
      (setf filter-type :after t type-filter): new method; compile predicate
      when type slot is set
      (payload-matches? type-filter t): if available call compiled predicate

--- src/filter/type-filter.lisp
+++ src/filter/type-filter.lisp
@@ -8,11 +8,16 @@
 
 (defclass type-filter (funcallable-filter-mixin
                        payload-matching-mixin)
-  ((type :initarg  :type
-         :type     (or list symbol)
-         :accessor filter-type
-         :documentation
-         "The type of matching events."))
+  ((type      :type     (or list symbol)
+              :accessor filter-type
+              :documentation
+              "The type of matching events.")
+   (predicate :type     (or null function)
+              :accessor filter-%predicate
+              :initform nil
+              :documentation
+              "Stores a compiled predicate implementing the filter or
+               nil if none is available."))
   (:metaclass closer-mop:funcallable-standard-class)
   (:default-initargs
    :type (missing-required-initarg 'type-filter :type))
@@ -22,8 +27,36 @@
 (service-provider:register-provider/class 'filter :type
   :class 'type-filter)
 
-(defmethod payload-matches? ((filter type-filter) (payload t) &key)
-  (typep payload (filter-type filter)))
+(defmethod shared-initialize :after ((instance   type-filter)
+                                     (slot-names t)
+                                     &key
+                                     (type nil type-supplied?))
+  (when type-supplied?
+    (setf (filter-type instance) type)))
+
+(defmethod (setf filter-type) :before ((new-value t)
+                                       (filter    type-filter))
+  (let+ (((&values function &ign failure?)
+          (block compile
+            (handler-bind (((and warning (not style-warning))
+                            (lambda (condition)
+                              (return-from compile (values nil condition t)))))
+              (let ((*error-output* (make-broadcast-stream)))
+                (with-compilation-unit (:override t)
+                  (compile nil `(lambda (payload) (typep payload ',new-value)))))))))
+    (when failure?
+      (error 'simple-type-error
+             :datum            new-value
+             :type             '(or cons symbol)
+             :format-control   "~@<~S is not a valid type specifier.~@:>"
+             :format-arguments (list new-value)))
+    (setf (filter-%predicate filter) (unless failure? function))))
+
+(defmethod payload-matches? ((filter type-filter) (payload t))
+  (if-let ((predicate (filter-%predicate filter)))
+    (locally (declare (type function predicate))
+      (funcall predicate payload))
+    (typep payload (filter-type filter))))
 
 (defmethod print-object ((object type-filter) stream)
   (print-unreadable-object (object stream :type t :identity t)
