07b8b19c9255e3b471c30bbca729d91f916990cf
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2016-07-06 16:14:07 +0200

    Allow specifying suite name in macros in test/filter/package.lisp
    * test/filter/package.lisp (define-basic-filter-tests): added keyword
      parameter suite-name for specifying the name of the test suite in
      which generated tests cases should be placed
      (define-filter-match-test): likewise

--- test/filter/package.lisp
+++ test/filter/package.lisp
@@ -50,62 +50,68 @@
     (check nil)
     (check t)))
 
-(defmacro define-basic-filter-tests ((class spec) construct-cases)
+(defmacro define-basic-filter-tests
+    ((class spec
+      &key
+      (suite-name (symbolicate class "-ROOT")))
+     construct-cases)
   "Define basic test cases for the filter class CLASS."
-  (let ((suite-name (symbolicate class "-ROOT")))
-    `(progn
-       (addtest (,suite-name
-                 :documentation
-                 ,(format nil "Test construction instances of the ~
-                               `~(~A~)' filter class."
-                          class))
-         construct
+  `(progn
+     (addtest (,suite-name
+               :documentation
+               ,(format nil "Test construction instances of the ~
+                             `~(~A~)' filter class."
+                        class))
+       construct
 
-         (ensure-cases (args expected)
-             ,construct-cases
+       (ensure-cases (args expected)
+           ,construct-cases
 
-           (case expected
-             (error
-              (ensure-condition error
-                (apply #'make-instance ',class args)))
-             (t
-              (apply #'make-instance ',class args)
-              (apply #'make-filter ,spec args)
-              (apply #'filter ,spec args)))))
+         (case expected
+           (error
+            (ensure-condition error
+              (apply #'make-instance ',class args)))
+           (t
+            (apply #'make-instance ',class args)
+            (apply #'make-filter ,spec args)
+            (apply #'filter ,spec args)))))
 
-       (addtest (,suite-name
-                 :documentation
-                 ,(format nil "Test method on `print-object' for the ~
-                               `~(~A~)' filter class."
-                          class))
-         print
+     (addtest (,suite-name
+               :documentation
+               ,(format nil "Test method on `print-object' for the ~
+                             `~(~A~)' filter class."
+                        class))
+       print
 
-         (mapc (lambda+ ((initargs &ign))
-                 (check-print (apply #'make-filter ,spec initargs)))
-               (remove t ,construct-cases :key #'second :test-not #'eq))))))
+       (mapc (lambda+ ((initargs &ign))
+               (check-print (apply #'make-filter ,spec initargs)))
+             (remove t ,construct-cases :key #'second :test-not #'eq)))))
 
-(defmacro define-filter-match-test ((class spec) cases)
+(defmacro define-filter-match-test
+    ((class spec
+     &key
+     (suite-name (symbolicate class "-ROOT")))
+     cases)
   "Define a test case for the `matches?' method of filter class
    CLASS."
-  (let ((suite-name (symbolicate class "-ROOT")))
-    `(addtest (,suite-name
-               :documentation
-               ,(format nil "Smoke test for the `~(~A~)' filter class."
-                        class))
-       smoke
+  `(addtest (,suite-name
+             :documentation
+             ,(format nil "Smoke test for the `~(~A~)' filter class."
+                      class))
+     smoke
 
-       (ensure-cases (filter-initargs event-spec expected)
-           ,cases
-         (let ((filter (apply #'filter (if (listp (first filter-initargs))
-                                           (list (list* ,spec filter-initargs))
-                                           (list* ,spec filter-initargs)))))
-           (call-with-filter-checking-thunk
-            (lambda (do-it)
-              (let ((result (funcall do-it)))
-                (ensure-same result expected
-                             :report    "~@<The filter ~S ~:[did not ~
-                                         match~;matched~] the event ~S, ~
-                                         but should~:[ not~;~].~@:>"
-                             :arguments (filter result event-spec expected))))
-            filter
-            event-spec))))))
+     (ensure-cases (filter-initargs event-spec expected)
+         ,cases
+       (let ((filter (apply #'filter (if (listp (first filter-initargs))
+                                         (list (list* ,spec filter-initargs))
+                                         (list* ,spec filter-initargs)))))
+         (call-with-filter-checking-thunk
+          (lambda (do-it)
+            (let ((result (funcall do-it)))
+              (ensure-same result expected
+                           :report    "~@<The filter ~S ~:[did not ~
+                                       match~;matched~] the event ~S, ~
+                                       but should~:[ not~;~].~@:>"
+                           :arguments (filter result event-spec expected))))
+          filter
+          event-spec)))))
